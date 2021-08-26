fd8a25ef8473f6e55777d17006f9442c831a1a97
youzi530 <2676272310@qq.com>
2021-08-26 18:13:13 +0800

    [lingqiao]feat: add chapter1-2 the configuration

---
+++ springBootLearning/chapter1-2/src/test/java/com/example/chapter12/Chapter12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
